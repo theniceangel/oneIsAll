0646754dc526323eeb0fc21af3eedc3c30ed207b
theniceangel <theniceangel@163.com>
2017-07-16 16:44:19 +0800

    opt@init_project20170716#:init project

---
+++ src/components/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    这是主页
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+<style scoped lang="stylus">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'common/style/index.styl'
+import fastclick from 'fastclick'
+
+/* eslint-disable no-unused-vars */
+import console from 'vconsole'
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
