2b448bfede6118f71ac950a65b51a3c43371d6b1
jizhi <theniceangel@163.com>
2017-08-14 21:51:22 +0800

    opt@complete_home#:test

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -51,19 +51,18 @@ function getCurrentDate () {
   return `${year}-${month}-${day}`
 }
 // 获取昨天的日期,返回格式为yyyy-mm-dd
-function GetYesterdayDate (currentDay, subtractionDayCount) {
-  let temp = new Date(Date.parse(currentDay.replace(/-/g, '/')))
-  temp.setDate(temp.getDate() - subtractionDayCount) // 获取减去subtractionDayCount天后的日期
-  let year = temp.getFullYear()
-  let month = padString((temp.getMonth() + 1).toString(), 2, 0)
-  var day = padString(temp.getDate().toString(), 2, 0)
-  console.log(`${year}-${month}-${day}`)
-  return `${year}-${month}-${day}`
-}
+// function GetYesterdayDate (currentDay, subtractionDayCount) {
+//   let temp = new Date(Date.parse(currentDay.replace(/-/g, '/')))
+//   temp.setDate(temp.getDate() - subtractionDayCount) // 获取减去subtractionDayCount天后的日期
+//   let year = temp.getFullYear()
+//   let month = padString((temp.getMonth() + 1).toString(), 2, 0)
+//   var day = padString(temp.getDate().toString(), 2, 0)
+//   console.log(`${year}-${month}-${day}`)
+//   return `${year}-${month}-${day}`
+// }
 export {
   countTime,
   findIndexInArray,
   getCurrentDate,
-  padString,
-  GetYesterdayDate
+  padString
 }
